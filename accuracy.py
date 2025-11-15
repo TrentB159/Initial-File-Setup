efdab1e22e3581d21e16814964325fa1c960e7ec
Trent Battisti <trentbattisti@gmail.com>
2025-11-14 19:42:16 -0800

    Gathered data from transfer learning

--- accuracy.py
+++ accuracy.py
@@ -13,8 +13,8 @@ STOCKFISH_PATH = r"C:\Users\p45mi\Downloads\stockfish-windows-x86-64-avx2\stockf
 
 # the input game file and the output CSV file
 # MUST BE CHNAGED MANUALLY UPON USE
-pgn_path = "newmodelsim.pgn"
-output_csv = "newmodelsim.csv"
+pgn_path = "newmodelsim(transfer_learning_original).pgn"
+output_csv = "newmodelsim(transfer_learning_original).csv"
 
 engine = chess.engine.SimpleEngine.popen_uci(STOCKFISH_PATH)
 results_list = []
